10844a42843488e40f9e2bea72868321afa7f74d
SDGLBL <lijieisguge@gmail.com>
2018-12-21 16:52:54 +0800

    服务器框架搭建ver1.2

---
+++ Server/src/commandUnzip.java
@@ -0,0 +1,10 @@
+/**
+ * 用于将获取的命令解密的类
+ */
+public class commandUnzip {
+    public commandUnzip(){
+    }
+    public void commandRead(String command){
+
+    }
+}
--- Server/src/creatServer.java
+++ Server/src/creatServer.java
@@ -1,9 +1,5 @@
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
-import javax.swing.text.BadLocationException;
-import javax.swing.text.SimpleAttributeSet;
-import javax.swing.text.StyleConstants;
-import javax.swing.text.StyledDocument;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -43,11 +39,13 @@ public class creatServer {
     public static void main(String[] args) {
         new creatServer();
     }
-    //服务端界面代码区域
+    /**
+     * 服务端GUI界面构造区域
+     */
     creatServer(){
         frame=new JFrame("游戏服务器");
         frame.add(new ServerJPanel());
-        frame.setSize(500,400);
+        frame.setSize(600,500);
         //设置服务器在屏幕正中央
         int screen_width = Toolkit.getDefaultToolkit().getScreenSize().width;
         int screen_height = Toolkit.getDefaultToolkit().getScreenSize().height;
@@ -81,23 +79,30 @@ public class creatServer {
                 }
                 //开启成功进行开启扫尾工作
                 contentArea.append("服务器开启成功");
+            }
+        });
+        //TODO:命令行输入确认监听
+        btn_send.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String inputCommand=txt_mes.getText().trim();//获取输入的命令
+                txt_mes.setText(null);//将命令行置空
+                contentArea.append(inputCommand+"\r\n");//信息框里面显示输入命令
 
             }
         });
     }
-
     /**
-     *
      * @param maxPlayer 最大可连接的玩家数目
      * @param port 服务器开启的监听端口
      * @throws BindException 抛出开启失败的错误情况
      */
     public void startServer(int maxPlayer,int port) throws BindException {
-        playerclients=new ArrayList<clientThread>();
         try {
-            serverSocket=new ServerSocket(port);
-            serverThread=new ServerThread(serverSocket,30);
-            serverThread.start();
+            playerclients=new ArrayList<clientThread>();//创建玩家列表
+            serverSocket=new ServerSocket(port);//创建服务Socket
+            serverThread=new ServerThread(serverSocket,30);//创建服务器线程
+            serverThread.start();//服务器线程开启
             isStart=true;
         } catch (BindException e) {
             e.printStackTrace();
@@ -111,6 +116,9 @@ public class creatServer {
         }
     }
 
+    /**
+     * 关闭服务器函数进行关闭和扫尾工作
+     */
     public void closeServer(){
         try{
         if(serverThread!=null) serverThread.interrupt();//服务端主线程如果存在则停止服务端主线程
@@ -130,29 +138,61 @@ public class creatServer {
     }
     public void sendToOther(){
         //TODO:转发给其它玩家函数
+
     }
     //服务器线程
     class ServerThread extends Thread{
         private ServerSocket serverSocket;
         private int maxplayer;
+        /**
+         * 服务器线程构造函数
+         * @param serverSocket 传入的serverSocket
+         * @param maxplayer 传入的最大在线人数
+         */
         public ServerThread(ServerSocket serverSocket,int maxplayer){
             this.serverSocket=serverSocket;
             this.maxplayer=maxplayer;
         }
+        /**
+         * 服务器线程不断循环等待客户端的连接
+         */
         public void run(){
             while(!this.isInterrupted()){
                 try {
-                    Socket socket=serverSocket.accept();
+                    Socket socket=serverSocket.accept();//得到一个客户端与服务器的Socket连接对象并保存
+                    if(playerclients.size()==maxplayer){//当前人数已满
+                        //TODO:服务器人数已满返回给现要连接的客户端信息
+                        socket.close();
+                        continue;//拒绝连接后跳出本次循环等待下次连接
+                    }
+                    clientThread aplayerClient=new clientThread(socket);//创建一个服务线程
+                    playerclients.add(aplayerClient);//将此服务线程压入playerclients中保存
+                    aplayerClient.start();//启动该服务线程
+                    listModel.addElement(aplayerClient.getGamer().getName());//在在线用户中添加新的用户
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
     }
-    //服务线程每个玩家分配一个
+
+    /**
+     * 玩家服务线程
+     */
     class clientThread extends Thread{
         private Gamer player;
         private Socket socket;
+        /**
+         * 获取此线程实例对象的Gamer
+         * @return
+         */
+        public Gamer getGamer(){
+            return player;
+        }
+        /**
+         * 玩家服务线程构造
+         * @param socket 属于实例对象(一位玩家)的Socket通道
+         */
         public clientThread(Socket socket){
             this.socket=socket;
             //TODO:服务线程构造函数待完成部分
@@ -162,7 +202,10 @@ public class creatServer {
             //TODO:服务线程run待完成
         }
     }
-    //ServerJPnel用于布局服务器GUI界面bylijie
+
+    /**
+     * ServerJPanel统一管理服务器界面布局
+     */
     class ServerJPanel extends JPanel{
         ServerJPanel(){
             initial();
