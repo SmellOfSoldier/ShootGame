d46ca57ec5988fbc7bc7d8a202ccfac5cd489a89
SmellOfSoldier <15574541819@163.com>
2018-12-22 18:32:37 +0800

    增添了游戏房间

--- src/Test.java
+++ src/Test.java
@@ -1,17 +1,51 @@
 import person.Person;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.List;
+import java.util.Vector;
 
 public class Test
 {
     public static void main(String[] args) {
+
         JFrame jFrame=new JFrame();
-        jFrame.setSize(200,200);
-        JFrame jFrame1=new JFrame();
-        jFrame1.setSize(200,200);
+        DefaultListModel<String> defaultListModel=new DefaultListModel<String>();
+        JList<String> clientJList=new JList<String>(defaultListModel);
+        jFrame.setSize(200,800);
         jFrame.setLocationRelativeTo(null);
-        jFrame1.setLocationRelativeTo(jFrame);
+        defaultListModel.addElement("老大124124124214");
+        defaultListModel.addElement("老三");
+        defaultListModel.addElement("老二");
+        JButton button=new JButton("踢出房间");
+        button.setSize(40,40);
+        button.setLocation(100,40);
+
+        clientJList.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+
+                List<String> list=clientJList.getSelectedValuesList();
+
+                for(String s:list)
+                {
+                    System.out.println(s);
+                }
+            }
+        });
+
+        JScrollPane jsp=new JScrollPane(clientJList);
+        jsp.setSize(200,200);
+        JPanel jPanel=new JPanel();
+        jPanel.setSize(200,800);
+        jPanel.setLayout(null);
+        jPanel.add(button);
+        jPanel.add(jsp);
+        jFrame.add(jPanel);
         jFrame.setVisible(true);
-        jFrame1.setVisible(true);
+
     }
 }
---
+++ src/view/Client.java
@@ -0,0 +1,86 @@
+package view;
+
+import java.io.Serializable;
+
+public class Client implements Serializable
+{
+    private String id;
+    private String password;
+    private boolean isOnline=false;
+    private  boolean isPlaying=false;
+    private GameRoom room;
+    /**
+     * Player构造函数
+     * @param id 玩家ID（唯一标识）
+     * @param password 玩家账号密码
+     */
+    public Client(String id, String password) {
+        this.id = id;
+        this.password = password;
+    }
+
+    /**
+     * 获取Player实例对象ID
+     * @return 返回Player实例对象ID
+     */
+    public String getId(){
+        return id;
+    }
+
+    /**
+     * 密码比对函数
+     * @param password 需要对比的密码
+     * @return 密码正确返回true 否则返回false
+     */
+    public boolean passwordEquals(String password){
+        if(this.password.equals(password)) return true;
+        else return false;
+    }
+
+    /**
+     * 设置玩家所在房间
+     * @param gameRoom 房间对象
+     */
+    public void setGameRoom(GameRoom gameRoom){
+        this.room=gameRoom;
+    }
+    /**
+     * 设置是否在线
+     * @param flag true在线 反之
+     */
+    public void setOline(boolean flag){
+        isOnline=flag;
+    }
+
+    /**
+     * 设置是否在游戏中（房间中）
+     * @param flag true游戏中 反之
+     */
+    public void setPlaying(boolean flag){
+        isPlaying=flag;
+    }
+    /**
+     * 检查是否在线
+     * @return true在线 反之
+     */
+    public boolean isOline(){
+        return isOnline;
+    }
+
+    /**
+     * 检查是否在游戏中（房间中）
+     * @return
+     */
+    public boolean isPlaying(){
+        return isPlaying;
+    }
+
+    /**
+     * 返回房间实例对象
+     * @return 房间实例索引
+     */
+    public GameRoom getRoom(){
+        return room;
+    }
+}
+
